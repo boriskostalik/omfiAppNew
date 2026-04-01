972d155662fc112366451ef1978d6561fbcb23ed
boriskostalik <boris.kostalik@student.ukf.sk>
2026-04-01 22:11:23 +0200

    Text change edit profile

--- resources/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -40,11 +40,6 @@ const closeModal = () => {
     <section class="space-y-6">
         <header>
             <h2 class="text-lg font-medium text-gray-900">Zmazať účet</h2>
-            <p class="mt-1 text-sm text-gray-600">
-                Po zmazaní účtu budú všetky jeho zdroje a údaje natrvalo
-                odstránené. Pred zmazaním účtu si prosím stiahnite všetky údaje,
-                ktoré si chcete ponechať.
-            </p>
         </header>
 
         <DangerButton @click="confirmUserDeletion">Zmazať účet</DangerButton>
